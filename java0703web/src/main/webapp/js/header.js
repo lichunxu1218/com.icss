7c9d559e83f29d98129ba0d8ee80b7a95ea7f0fb
lichunxu <836125851@qq.com>
2022-07-04 12:22:15 +0800

    删除

--- java0703web/src/main/webapp/js/header.js
+++
@@ -1,8 +0,0 @@
-var username = sessionStorage.key("username");
-if(username==null){
-    document.getElementsByClassName(".login").style.display ="none";
-    document.getElementsByClassName(".login_out").style.display ="";
-}else{
-    document.getElementsByClassName(".login").style.display ="";
-    document.getElementsByClassName(".login_out").style.display ="none";
-}
